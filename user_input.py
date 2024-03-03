827f18538913a3591ea2f1e76ac62ccaf65f031f
ufanete21 <116494078+ufanete21@users.noreply.github.com>
2024-03-03 16:25:41 +0100

    Add files via upload
    My first python assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('What is your name?')
+age = input('How old are you?')
+location = input('Where do you live?')
+print("Hello", (name), "you are", (age), "years old and live in", (location))
