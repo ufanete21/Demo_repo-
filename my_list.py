a41b7c33ed4421276f0c8f643c2bcb06a0a7854a
ufanete21 <116494078+ufanete21@users.noreply.github.com>
2024-03-08 16:02:24 +0100

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,14 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+#print(my_list)
+list2 = [50, 60, 70]
+my_list.extend(list2)
+#print(my_list)
+my_list.remove(70)
+#print(my_list)
+my_list.sort
+print(my_list.index(30))
